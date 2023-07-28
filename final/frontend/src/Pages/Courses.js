b4592f3baeae3e62e24a8d504df495e05eba60ba
PavanPriyan <125962273+PavanPriyan@users.noreply.github.com>
2023-07-28 12:19:25 +0530

    Add files via upload

---
+++ final/frontend/src/Pages/Courses.js
@@ -0,0 +1,52 @@
+import React from "react";
+import './Courses.css';
+import Ap from '../Components/Card';
+import Apj from '../Components/Cardd';
+function Courses(){
+    return(
+        <div>
+     
+        <br/>
+          <h1 className='er'>Courses</h1>
+  
+  
+  
+  
+        
+  
+  
+        <div id="section2" className="section">
+  
+        <br/>
+        <Ap/><br/>
+        <Apj/><br/>
+            
+  
+         
+  
+  
+  
+  
+  
+        </div>
+  
+  
+  
+  
+  
+      </div>
+  
+        
+        
+    
+              
+  
+        
+  
+  
+  
+    )
+  }
+    
+
+export default Courses
---
+++ final/frontend/src/Pages/home.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import Nav from '../Nav';
+import './Home.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Margin } from '@mui/icons-material';
+
+
+
+
+const Home = () => {
+  return (
+    <>
+    <Nav />
+    
+    <div>
+     
+      <br/>
+        <h1 className='er' >Welome to our Website</h1>
+<img  src='https://idreamcareer.com/wp-content/uploads/2022/05/courses-after-graduation.webp'></img>
+
+    </div>
+
+      
+      
+      </>
+            
+
+      
+
+
+
+  )
+}
+
+export default Home
